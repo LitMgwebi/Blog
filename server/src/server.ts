46ee564d68ef56506e8f1b6a7d7a5cf5192ad566
Lithi <lithim11@gmail.com>
2023-02-06 08:37:13 +0200

    Terminating Host connection on server

--- server/src/server.ts
+++ server/src/server.ts
@@ -17,7 +17,6 @@ import corsOptions from "./config/corsOptions";
 
 //#region Server setup
 const port: any = process.env.PORT;
-const host: string = process.env.HOST;
 const dbURL: string = process.env.DBURL;
 
 const server = express();
@@ -35,23 +34,23 @@ server.use('/user', userRouter);
 
 //#region Connecting M.E.R.N
 server.use(express.static(path.join(__dirname, '../../client/build')));
-server.all('*', (req, res) => {
-     res.status(404)
-     if (req.accepts('html')) {
-         res.sendFile(path.join(__dirname, 'views', '404.html'))
-     } else if (req.accepts('json')) {
-         res.json({ message: '404 Not Found' })
-     } else {
-         res.type('txt').send('404 Not Found')
-     }
- })
+// server.all('*', (req, res) => {
+//      res.status(404)
+//      if (req.accepts('html')) {
+//          res.sendFile(path.join(__dirname, 'views', '404.html'))
+//      } else if (req.accepts('json')) {
+//          res.json({ message: '404 Not Found' })
+//      } else {
+//          res.type('txt').send('404 Not Found')
+//      }
+//  })
 //#endregion
 
 //#region Database Setup
 connect(dbURL)
      .then(() => {
           log.info(`Connected to Database`);
-          server.listen(port, host, () => {
+          server.listen(port, () => {
                log.info(`Server running on: ${port}`);
           })
      }).catch((err: Error) => {
